f842035972c5e36d2bfc5102f7517a0fe2a1cf6d
roum02 <roum02@ewhain.net>
2021-10-08 02:16:48 +0900

    profile layout 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import GlobalStyle from "./Asset/GlobalStyle";
 import Template from "./Components/Template";
 import TopBar from "./Components/TopBar";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Template>
         <TopBar />
+        <Profile />
       </Template>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/Profile.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import Apeach from "../Asset/Apeach.jpg";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 6rem;
+  background-color: #9bbbd4;
+  margin: 0px;
+  box-shadow: 0px 8px 10px #556677;
+`;
+
+const ProfileImage = styled.img`
+  display: flex;
+  border-radius: 50px;
+  width: 5rem;
+  height: 5rem;
+  object-fit: cover;
+`;
+
+const ProfileText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1rem;
+  p,
+  h3 {
+    margin: 0px;
+  }
+`;
+
+function Profile() {
+  return (
+    <Wrapper>
+      <ProfileImage src={Apeach} alt="프로필" />
+      <ProfileText>
+        <h3>어피치</h3>
+        <p>현재 활동 중</p>
+      </ProfileText>
+    </Wrapper>
+  );
+}
+
+export default Profile;
