496bd39a99350a1c2c61e32647699b9268a3ea73
roum02 <roum02@ewhain.net>
2021-10-08 16:12:36 +0900

    메시지 기능 구현 완료

--- src/Components/ProfileBox.js
+++ src/Components/ProfileBox.js
@@ -1,27 +1,13 @@
 import React from "react";
-import styled from "styled-components";
 import ProfileItem from "./ProfileItem";
-import Apeach from "../Asset/Apeach.jpg";
-import Neo from "../Asset/Neo.jpg";
-
-const Wrapper = styled.div`
-  /* display: flex;
-  align-items: center;
-  height: 6rem;
-  width: 100%;
-  background-color: #9bbbd4;
-  box-shadow: 0px 8px 10px #556677;
-  position: absolute;
-  z-index: 99; */
-`;
 
 function ProfileBox({ users, onToggle }) {
   return (
-    <Wrapper>
+    <>
       {users.map((user) => (
         <ProfileItem user={user} key={user.id} onToggle={onToggle} />
       ))}
-    </Wrapper>
+    </>
   );
 }
 
