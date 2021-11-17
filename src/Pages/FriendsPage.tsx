894301958375f861adce20bb9dad441d68fb8ef7
roum02 <roum02@ewhain.net>
2021-11-18 02:44:59 +0900

    search 기능 구현

--- src/Pages/FriendsPage.tsx
+++ src/Pages/FriendsPage.tsx
@@ -38,7 +38,7 @@ const Input = styled.input`
 
 const FriendsPage = () => {
   const [show, setShow] = useState(false);
-  //const [searchTerm, setSearchTerm] = useState("");
+  const [searchFriends, setSearchFriends] = useState("");
   const [friends] = useState([
     {
       id: 1,
@@ -51,10 +51,17 @@ const FriendsPage = () => {
       id: 2,
       photo: "https://t1.daumcdn.net/cfile/blog/260CA94D57CC186328",
       name: "네오",
-      text: "교수님 전 잘 듣고 있어요",
+      text: "집가고싶네오",
     },
     {
       id: 3,
+      photo:
+        "https://item.kakaocdn.net/do/d97f27efd1d10d84215842e2e12752939f5287469802eca457586a25a096fd31",
+      name: "튜브",
+      text: "교수님 미워요",
+    },
+    {
+      id: 4,
       photo:
         "https://i.pinimg.com/originals/78/a2/4e/78a24e8c7ee392147864fd1059cf74da.jpg",
       name: "무지",
@@ -64,6 +71,27 @@ const FriendsPage = () => {
 
   function onClick() {
     setShow(!show);
+  }
+
+  function FindFriends() {
+    let data = friends.filter((data) => data.name.indexOf(searchFriends) > -1);
+    let friendsList: {
+      id: number;
+      photo: string;
+      name: string;
+      text: string;
+    }[] = [];
+    data.map((friend, i) => friendsList.push(data[i]));
+    console.log(friendsList);
+
+    return friendsList.map((friend) => (
+      <FriendsItem
+        key={friend.id}
+        name={friend.name}
+        photo={friend.photo}
+        text={friend.text}
+      />
+    ));
   }
 
   return (
@@ -80,22 +108,22 @@ const FriendsPage = () => {
           <Input
             style={{ display: "block" }}
             type="text"
-            // onChange={(e) => {
-            //   setSearchTerm(e.target.value);
-            // }}
+            onChange={(e) => {
+              setSearchFriends(e.target.value);
+            }}
           />
         ) : (
           <Input style={{ display: "none" }} />
         )}
-
-        {friends.map((friend) => (
+        {FindFriends()}
+        {/* {friends.map((friend) => (
           <FriendsItem
             key={friend.id}
             name={friend.name}
             photo={friend.photo}
             text={friend.text}
           />
-        ))}
+        ))} */}
       </FriendsBox>
     </Wrapper>
   );
